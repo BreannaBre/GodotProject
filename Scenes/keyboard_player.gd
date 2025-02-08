7ba4682cb2b3b1935ee28bca1038ee64fd0c41c2
Breanna Breedlove <breanna.may.breedlove@gmail.com>
2025-02-08 11:08:06 -0600

    keyboard character has jump and run

--- Scenes/keyboard_player.gd
+++ Scenes/keyboard_player.gd
@@ -7,20 +7,20 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity += get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
 	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("Player1Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
-	#reset velocity
-	velocity = Vector2(0,0)
-	#move based on input
+	#reset hoizontal velocity
+	velocity.x = 0
+	#direction based on input
 	if Input.is_action_pressed("Player1Left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("Player1Right"):
 		velocity.x += 1
 	velocity.x = velocity.x*SPEED
 	
-	move_and_collide(velocity * delta)
+	move_and_slide()
