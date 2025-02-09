63d671b52531ef9dec54f729f00d74706ad5289c
Robert Boerwinkle <rfboerwinkle@gmail.com>
2025-02-09 15:02:41 -0600

    doing the renaming in 2 parts so git knows whats up

--- src/gun_room.gd
+++ src/gun_room.gd
@@ -2,18 +2,18 @@ class_name GunRoom
 extends Node2D
 
 
-var gunPicture:Sprite2D
+var gun_picture:Sprite2D
 const GUN_SCALE := Vector2(0.6, 0.6)
 
 
 func _ready() -> void:
-	gunPicture = get_node("%GunPicture")
+	gun_picture = get_node("%GunPicture")
 
 func _process(_delta: float) -> void:
 	pass
 
 func train(rotation: float) -> void:
-	gunPicture.transform = Transform2D(rotation, GUN_SCALE, 0, Vector2(0,0))
+	gun_picture.transform = Transform2D(rotation, GUN_SCALE, 0, Vector2(0,0))
 
 func fire(rotation: float) -> void:
-	gunPicture.transform = Transform2D(rotation, GUN_SCALE, 0, Vector2(0,0))
+	gun_picture.transform = Transform2D(rotation, GUN_SCALE, 0, Vector2(0,0))
--- src/main.gd
+++ src/main.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-const gunRoomScene := preload("res://Scenes/gun_room.tscn")
+const GUN_ROOM_SCENE := preload("res://scenes/gun_room.tscn")
 var rooms: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	rooms = get_node("%RoomsNode")
-	add_room(gunRoomScene, Vector2(300,300))
-	add_room(gunRoomScene, Vector2(300+181,300))
-	add_room(gunRoomScene, Vector2(300+181,300+181))
-	add_room(gunRoomScene, Vector2(300,300+181))
-	add_room(gunRoomScene, Vector2(300,300-181))
+	add_room(GUN_ROOM_SCENE, Vector2(300,300))
+	add_room(GUN_ROOM_SCENE, Vector2(300+181,300))
+	add_room(GUN_ROOM_SCENE, Vector2(300+181,300+181))
+	add_room(GUN_ROOM_SCENE, Vector2(300,300+181))
+	add_room(GUN_ROOM_SCENE, Vector2(300,300-181))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -25,6 +25,6 @@ func _input(event: InputEvent):
 			room.train(room.transform.origin.angle_to_point(event.position))
 
 func add_room(room: PackedScene, coords: Vector2) -> void:
-	var newRoom:Node2D = room.instantiate()
-	newRoom.transform.origin = coords
-	rooms.add_child(newRoom)
+	var new_room:Node2D = room.instantiate()
+	new_room.transform.origin = coords
+	rooms.add_child(new_room)
