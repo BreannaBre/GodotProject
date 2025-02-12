9846c41c87a9f72edb86c3ba2261b15649b589b1
Malizma333 <malizma333@gmail.com>
2025-02-12 14:45:54 -0600

    Flip character on move

--- src/keyboard_player.gd
+++ src/keyboard_player.gd
@@ -18,8 +18,10 @@ func _physics_process(delta: float) -> void:
 	velocity.x = 0
 	#direction based on input
 	if Input.is_action_pressed("Player1Left"):
+		get_node("%Fishcat").flip_h = true
 		velocity.x -= 1
 	if Input.is_action_pressed("Player1Right"):
+		get_node("%Fishcat").flip_h = false
 		velocity.x += 1
 	velocity.x = velocity.x*SPEED
 
