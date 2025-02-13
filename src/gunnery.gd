835a7862cd64d4925fa699ddbc7d1546c133d595
Breanna Breedlove <breanna.may.breedlove@gmail.com>
2025-02-13 01:43:49 -0600

    added reattatch image to rooms

--- src/gunnery.gd
+++ src/gunnery.gd
@@ -11,6 +11,9 @@ func _ready() -> void:
 	var body_unsafe := get_node("%Body")
 	assert(body_unsafe is RigidBody2D, "Somebody's been mucking with the gunnery nodes")
 	body = body_unsafe as RigidBody2D
+	var repair_sign_unsafe := get_node("%RepairSign")
+	assert(repair_sign_unsafe is Sprite2D, "Somebody's been mucking with the gunnery nodes")
+	repair_sign = repair_sign_unsafe
 	default_ready()
 
 func _process(delta: float) -> void:
@@ -18,6 +21,7 @@ func _process(delta: float) -> void:
 	if state == ATTACHED or state == REPAIRING:
 		var new_rotation := body.transform.origin.angle_to_point(target)
 		gun_picture.transform = Transform2D(new_rotation, GUN_SCALE, 0, Vector2(0,0))
+		
 
 func _physics_process(delta: float) -> void:
 	default_physics_process(delta)
--- src/room.gd
+++ src/room.gd
@@ -13,6 +13,7 @@ var tube: RID
 var welders: Array[RID]
 var host: RigidBody2D
 var reattach_work: float = 0
+var repair_sign: Sprite2D
 
 # This sets up defaults for the RigidBody2D, so make sure body has been
 # properly assigned!
@@ -22,11 +23,15 @@ func default_ready() -> void:
 	body.collision_layer = 0b1000
 	body.collision_mask  = 0b1100
 	tube = PhysicsServer2D.joint_create()
+	if repair_sign != null:
+		repair_sign.hide()
 
 func default_process(delta: float) -> void:
 	if state == REATTACHING:
 		reattach_work -= delta * welders.size()
+		repair_sign.show()
 		if reattach_work <= 0:
+			repair_sign.hide()
 			set_state(REPAIRING)
 	elif state == REPAIRING:
 		breakaway -= delta * welders.size() * 20
