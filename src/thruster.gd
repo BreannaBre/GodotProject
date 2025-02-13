1b6586f232e05f901f2f90956eb8c91909380174
Breanna Breedlove <breanna.may.breedlove@gmail.com>
2025-02-13 01:51:24 -0600

    added repair images but like actually

--- src/thruster.gd
+++ src/thruster.gd
@@ -5,9 +5,6 @@ func _ready() -> void:
 	var body_unsafe := get_node("%Body")
 	assert(body_unsafe is RigidBody2D, "Somebody's been mucking with the thruster nodes")
 	body = body_unsafe as RigidBody2D
-	var repair_sign_unsafe := get_node("%RepairSign")
-	assert(repair_sign_unsafe is Sprite2D, "Somebody's been mucking with the thruster nodes")
-	repair_sign = repair_sign_unsafe
 	default_ready()
 
 func _process(delta: float) -> void:
