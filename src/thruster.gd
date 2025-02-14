6cc79eed2b7713b4e037c507077ba35677bf6292
Breanna Breedlove <breanna.may.breedlove@gmail.com>
2025-02-13 20:57:29 -0600

    working animated thrusters (kinda goofy)

--- src/thruster.gd
+++ src/thruster.gd
@@ -1,7 +1,18 @@
 class_name Thruster
 extends Room
 
+var thruster_left: AnimatedSprite2D
+var thruster_right: AnimatedSprite2D
+
 func _ready() -> void:
+	var thruster_left_unsafe := get_node("%ThrusterLeft")
+	assert(thruster_left_unsafe is AnimatedSprite2D, "Somebody's been mucking with the thruster nodes")
+	thruster_left = thruster_left_unsafe as AnimatedSprite2D
+	thruster_left.play()
+	var thruster_right_unsafe := get_node("%ThrusterRight")
+	assert(thruster_right_unsafe is AnimatedSprite2D, "Somebody's been mucking with the thruster nodes")
+	thruster_right = thruster_right_unsafe as AnimatedSprite2D
+	thruster_right.play()
 	var body_unsafe := get_node("%Body")
 	assert(body_unsafe is RigidBody2D, "Somebody's been mucking with the thruster nodes")
 	body = body_unsafe as RigidBody2D
