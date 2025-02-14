8067aed31817538e7377974ef124d01f12fdfbd0
Breanna Breedlove <breanna.may.breedlove@gmail.com>
2025-02-13 21:45:42 -0600

    added restart button (doesn't work)

--- src/game_over_process.gd
+++ src/game_over_process.gd
@@ -10,3 +10,6 @@ func _ready() -> void:
 func set_time(value: float) -> void:
 	timer = value
 	label.text = "Time Survived: " + str(value) + " seconds"
+
+func _on_restart_button_pressed() -> void:
+	pass # Replace with function body.
